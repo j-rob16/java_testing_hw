5d974f0890168541373488b9bd955e7e53960814
James R <jimmer1609@gmail.com>
2022-08-02 12:45:58 +0900

    finished printer extensions

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,27 @@
 public class Printer {
+
+    private int availablePaper;
+    private int toner = 5000;
+    public void addPaper(int sheets) {
+        availablePaper += sheets;
+    }
+
+    public int getSheets() {
+        return availablePaper;
+    }
+
+    public void print(int pages, int copies) {
+        int printRequest = pages * copies;
+        if (printRequest <= availablePaper && printRequest <= toner) {
+        availablePaper -= printRequest;
+        toner -= printRequest;
+        System.out.println("Printed");
+        } else {
+            System.out.println("Sorry, cannot print what you need.");
+        }
+    }
+
+    public int getToner() {
+        return toner;
+    }
 }
--- src/test/java/TestPrinter.java
+++ src/test/java/TestPrinter.java
@@ -1,2 +1,52 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestPrinter {
+
+    Printer printer;
+
+    @Before
+    public void before() {
+        printer = new Printer();
+        printer.addPaper(300);
+    }
+
+    @Test
+    public void newPrinterHasNoPaper() {
+        Printer newPrinter = new Printer();
+        assertEquals(0, newPrinter.getSheets());
+    }
+
+    @Test
+    public void printOnePage() {
+        printer.print(1, 1);
+        assertEquals(299, printer.getSheets());
+    }
+
+    @Test
+    public void printThirtyPages() {
+        printer.print(10, 3);
+        assertEquals(270, printer.getSheets());
+    }
+
+    @Test
+    public void printTooManyPages() {
+        printer.print(320, 1);
+        assertEquals(300, printer.getSheets());
+    }
+
+    @Test
+    public void reduceTonerBy10() {
+        printer.print(10, 1);
+        assertEquals(4990, printer.getToner());
+    }
+
+    @Test
+    public void notEnoughTonerToPrint() {
+        printer.addPaper(5500);
+        printer.print(5100, 1);
+        assertEquals(5000, printer.getToner());
+    }
 }
